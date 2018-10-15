4f50c31f898a106dd5f478bb5120115999745bd6
BenWoolf <44166756+BenWoolf@users.noreply.github.com>
2018-10-15 16:19:50 +0100

    Add files via upload

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<LifeCount>().lives = 0;
+
+            Destroy(gameObject);
+        }
+
+    }
+}
